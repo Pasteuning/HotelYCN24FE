0d8017625bf698f6ddcea156fb33a7e6f96316d3
leslie pasteuning <lesliepasteuning@leslies-MacBook-Pro.local>
2024-01-19 16:03:27 +0100

    Fe naar BE sturen

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function getAllHotels(){
             hotelhtml+=`
             <tr>
                 <td>${hotels[i].id}</td>
+                <td>${hotels[i].name}</td>
                  <td>${hotels[i].street}</td>
                  <td>${hotels[i].houseNumber}</td>
                  <td>${hotels[i].zipCode}</td>
@@ -20,4 +21,27 @@ function getAllHotels(){
         document.getElementById("hotels").innerHTML=hotelhtml
     })
     
+}
+
+function createHotel(){
+    let hotel =  {
+        "street": document.getElementById("street").value,
+        "houseNumber": document.getElementById("houseNumber").value,
+        "zipCode": document.getElementById("zipCode").value,
+        "city": document.getElementById("city").value,
+        "country": document.getElementById("country").value,
+        "name": document.getElementById("name").value
+    }
+    fetch("http://127.0.0.1:8080/hotel", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(hotel),
+    })
+    //.then(res => res.json())
+    // .then(antwoord =>{
+    //     alert("het is gelukt")
+    //      })
+
 }
