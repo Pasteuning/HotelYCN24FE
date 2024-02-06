a2bbb30e3bd39353b4af21e6bbe63d865ef57adf
stefanrhodenborgh <srhodenborgh@gmail.com>
2024-02-06 10:35:48 +0100

    search rooms

--- resources/js/reservations.js
+++ resources/js/reservations.js
@@ -1,18 +1,27 @@
 //Startup script:
 document.addEventListener('DOMContentLoaded', async function () {
+    // Hoteldropdown vullen met hotels
     await getAllHotels().then(hotels => {
         populateDropdown(hotels, "hotelDropdown", 1)
         populateDropdown(hotels, "hotelDropdown2", 1)
-        });
-        getAllReservations();
-    
+    });
+    getAllReservations();
+
+    displayRooms();
+
+    // Datum ophalen in format: YYYY-MM-DD
+    // Minimumdatum van checkin op vandaag zetten
+    let today = new Date();
+    document.getElementById("checkIn").min = today.toISOString().split('T')[0]; 
 });
+
 
 
 function getAllHotels() {
     return fetch("http://127.0.0.1:8080/allhotels")
     .then(hotels => hotels.json());
 }
+
 
 function populateDropdown(items, elementId, setValue) {
     //methode om een dropdown(elementId) te vullen met items en de selector op een item te zetten
@@ -27,6 +36,33 @@ function populateDropdown(items, elementId, setValue) {
     });
     dropdown.value = setValue;
 }
+
+
+async function displayRooms() {
+    //methode om alle kamers uit de hotel dropdown te laten zien.
+    //methode werkt op onchange, dus wordt aangeroepen als de hotel dropdown waarde verandert.
+    const hotelId = document.getElementById("hotelDropdown2").value;
+
+    await fetch("http://127.0.0.1:8080/hotel/" + hotelId + "/rooms")
+    .then(rooms => rooms.json())
+    .then(rooms => {
+        let roomshtml = `
+            <option value="unassigned">Unassigned</option>
+            `
+        for (let i=0; i<rooms.length; i++) {
+            roomshtml +=`
+            <option value="${rooms[i].id}">${rooms[i].id}</option>
+            `
+        }
+        
+        document.getElementById("roomDropdown").innerHTML = roomshtml;
+        document.getElementById("roomDropdown").value = "unassigned";
+    })
+}
+
+
+
+
 
 async function getAllReservations(){
     let sort = document.getElementById("sort").value;
@@ -79,49 +115,72 @@ async function getAllReservations(){
 
 
 async function createReservation() {
-    let reservation =  {
-        "ciDate": document.getElementById("checkIn").value,
-        "coDate": document.getElementById("checkOut").value,
-        "adults": document.getElementById("adults").value,
-        "children": document.getElementById("children").value,     
-    }
-    const res = await fetch("http://127.0.0.1:8080/createreservation", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
+    let resDTO =  {
+        "hotelId": document.getElementById("hotelDropdown2").value,
+        "roomId": document.getElementById("roomDropdown").value,
+        "reservation": {
+            "ciDate": document.getElementById("checkIn").value,
+            "coDate": document.getElementById("checkOut").value,
+            "adults": document.getElementById("adults").value,
+            "children": document.getElementById("children").value,    
         },
-        body: JSON.stringify(reservation),
-    });
+        "userId": 3,
+    };
+    console.log(resDTO.reservation.ciDate);
 
-    // Check if the response status is OK (200)
-    if (res.ok) {
+    if (resDTO.reservation.ciDate == null) {
+        alert("Please fill in check-in date")
+    } else if (resDTO.reservation.coDate == null) {
+        alert("Please fill in check-out date")
+    } else if (resDTO.reservation.adults == null) {
+        alert("Please fill in the number of adults")
+    } else if (resDTO.reservation.children == null) {
+        alert("Please fill in the number of children")
+    } else {
+        const res = await fetch("http://127.0.0.1:8080/createreservation", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(resDTO),
+        });
+
         const createdReservation = await res.json();
-        console.log(createdReservation);
-        assignRoom(createdReservation);
-        assignUser(createdReservation);
-        alert("Reservation successfully created");
         getAllReservations();
-    }
+        // if (createdReservation !== null) {
+            
+        //     console.log(createdReservation);
+        //     assignRoom(createdReservation);
+        //     assignUser(createdReservation);
+        //     alert("Reservation successfully created");
+            
+}
 }
 
 async function assignRoom(reservation) {
-    let roomId = document.getElementById("room").value;
-    console.log(reservation.id);
-    await fetch("http://127.0.0.1:8080/reservations/assignroom?reservationId=" + reservation.id + "&roomId=" + roomId, {
+    let roomId = document.getElementById("roomDropdown").value;
+    if (roomId !== null) {
+        console.log(reservation.id);
+        await fetch("http://127.0.0.1:8080/reservations/assignroom?reservationId=" + reservation.id + "&roomId=" + roomId, {
         method: "POST", 
         headers: {
             "Content-Type": "application/json",
         }})
+    }
+    
 }
 
 async function assignUser(reservation) {
     let userId = document.getElementById("user").value;
-    await fetch("http://127.0.0.1:8080/reservations/assignuser?reservationId=" + reservation.id + "&userId=" + userId, {
+    if (userId !== null) {
+        await fetch("http://127.0.0.1:8080/reservations/assignuser?reservationId=" + reservation.id + "&userId=" + userId, {
         method: "POST", 
         headers: {
             "Content-Type": "application/json",
         }})
+    }
 }
+
 
 async function deleteReservation(reservationId) {
     await fetch("http://localhost:8080/deletereservation/" + reservationId);
@@ -129,8 +188,35 @@ async function deleteReservation(reservationId) {
     getAllReservations();
 }
 
-async function editReservation() {
-    //TO BE CONTINUED!!!
+async function editReservation(reservationId, hotelId) {
+    await fetch("http://localhost:8080/editreservation/" + reservationId)
+    .then(res => res.json())
+    .then(reservation => {
+        let form = `
+        <h2>Edit Room</h2>
+        <label>Hotel: </label>
+        <select id="editHotelDropdown"></select><br>
+
+        <label>Room type: </label>
+        <select id="editRoomTypeDropdown" value="${reservation.roomType}">
+            <option value="0">Single</option>
+            <option value="1">Double</option>
+            <option value="2">Family</option>
+        </select><br>
+
+        <label>Number of beds</label>
+        <input type="text: " id="editNoBeds" value="${reservation.noBeds}"><br>
+        <label>Price: </label>
+        <input type="text" id="editPrice" value="${reservation.price}"><br>
+        <button onclick="submitRoomForm(${reservation.id})">Save changes</button>      
+        `
+
+        // getAllHotels().then(hotels => {
+        //     populateDropdown(hotels, "editHotelDropdown", hotelId);
+        //     });
+        
+        // document.getElementById("editRoom").innerHTML = form;
+    });
 }
 
 function setMinCheckOutDate() {
