f46cf5e46553cb74bf282d28974a91feb15dca56
leslie pasteuning <lesliepasteuning@leslies-MBP.home>
2024-02-07 20:08:35 +0100

    home + about us

---
+++ resources/js/home.js
@@ -0,0 +1,110 @@
+//Startup script:
+document.addEventListener('DOMContentLoaded', async function () {
+    // Hoteldropdown vullen met hotels
+    await getAllHotels().then(hotels => {
+        populateDropdown(hotels, "hotelDropdown", 1)
+    });
+});
+
+function getAllHotels() {
+    return fetch("http://127.0.0.1:8080/allhotels")
+    .then(hotels => hotels.json());
+}
+
+function populateDropdown(items, elementId, setValue) {
+    //methode om een dropdown(elementId) te vullen met items en de selector op een item te zetten
+    // const dropdown = document.getElementById(elementId);
+    const dropdown = document.getElementById(elementId);
+
+    items.forEach(item => {
+        
+        const option = document.createElement("option");
+        option.value = item.id;
+        option.textContent = item.name;
+        dropdown.appendChild(option);
+    });
+    dropdown.value = setValue;
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Datum ophalen in format: YYYY-MM-DD
+    // Minimumdatum van checkin op vandaag zetten
+    let today = new Date();
+    document.getElementById("checkIn").min = today.toISOString().split('T')[0];
+
+    // Minimumdatum van checkout op morgen zetten
+    let tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+    document.getElementById("checkOut").min = tomorrow.toISOString().split('T')[0];
+
+    // On load checkin- en out datum zetten
+    document.getElementById("checkIn").value = today.toISOString().split('T')[0];
+    document.getElementById("checkOut").value = tomorrow.toISOString().split('T')[0];
+
+    // On load adults value op 2, children op 0 zetten
+    document.getElementById("adults").value = 2;
+    document.getElementById("children").value = 0;
+
+});
+
+
+function setMinCheckOutDate(){
+    // Minimumdatum van checkout op volgende dag zetten indien checkin na checkout is
+    let checkInDate = new Date(document.getElementById("checkIn").value);
+    let checkOutDate = new Date(document.getElementById("checkOut").value);
+    
+    // Minimumdatum checkOut beweegt mee met de checkIn
+    let nextDay = new Date(checkInDate);
+    nextDay.setDate(checkInDate.getDate() + 1);   
+    document.getElementById("checkOut").min = nextDay.toISOString().split('T')[0];
+    
+    // Zet de waarde op de volgende dag t.o.v. checkIn indien de checkIn datum na de checkOut is geprikt
+    if (checkInDate >= checkOutDate) {    
+        document.getElementById("checkOut").value = nextDay.toISOString().split('T')[0];
+    }
+}
+
+async function searchRooms() {
+    // Elementen ophalen
+    const hotelDropdown = document.getElementById("hotelDropdown");
+    const checkInInput = document.getElementById("checkIn");
+    const checkOutInput = document.getElementById("checkOut");
+    const adultsInput = document.getElementById("adults");
+    const childrenInput = document.getElementById("children");
+    const roomTypeInput = document.getElementById("roomTypeDropDown");
+
+    // HotelName krijgen uit de textwaarde van geselecteerde optie in hotelDropdown
+    const selectedOption = hotelDropdown.options[hotelDropdown.selectedIndex];
+    const hotelName = selectedOption.textContent;
+
+    // RoomType krijgen uit de textwaarde van geselecteerde optie in roomTypeDropdown
+    const selectedOptionRoom = roomTypeDropdown.options[roomTypeDropdown.selectedIndex];
+    const roomTypeChoice = selectedOption.textContent;
+
+    // Parameters om de beschikbare kamers mee te zoeken
+    const hotelId = hotelDropdown.value;
+    const ciDate = checkInInput.value;
+    const coDate = checkOutInput.value;
+    const adults = adultsInput.value;
+    const children = childrenInput.value;
+    const roomType = roomTypeInput.value;
+
+    // Object maken van de query om later door te geven
+    const query = {
+        hotelId,
+        hotelName,
+        ciDate,
+        coDate,
+        adults,
+        children,
+        roomType
+    };
+
+
+    // Stopt deze functie als er minder dan 1 volwassene is ingevoerd
+    if (adults < 1) {
+        alert("Rooms may not be reserved without an adult present")
+        return;
+    }
+}
